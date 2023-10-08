73527fc687baa035a76dba9fb30ada3798571bd9
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-10-08 16:41:56 +0300

    local commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="log" />
-        <p>Edit and save to reload.</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Edit and save to reload</p>
         <a
           className="App-link"
           href="https://reactjs.org"
